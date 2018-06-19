5bdd410fdd3526d0a4f6ed60112c71e72271fe3f
BrandonHoeft <brandonmhoeft@gmail.com>
2018-06-18 20:40:19 -0500

    in which our hero adds a doc string, renames  function parameter longest_streak to be best_streak  so as not to confuse the with the identically named  func.
     kept some lines for testing in there.
    
     Also, can try and improve code maybe by using
     enumerate() in my for loop.

--- longest_streak.py
+++ longest_streak.py
@@ -1,46 +1,38 @@
 def longest_streak(var, value):
-
+    """Given a list, and a scalar value to be searched for in the list, return
+    the maximum number of times this value occurs consecutively in the list."""
+    
     current_streak = 0
-    longest_streak = 0
-    longest_streak_loc = 0
+    best_streak = 0
+    best_streak_loc = 0
 
     for i in range(len(var)):
         if var[i] == value:
             current_streak += 1
-            longest_streak = max(longest_streak, current_streak)
-            print('if... ' + str(i) + ', ' + str(current_streak) + ', ' + str(longest_streak))
+            best_streak = max(best_streak, current_streak)
 
         else:
-            longest_streak = max(longest_streak, current_streak)
+            best_streak = max(best_streak, current_streak)
             current_streak = 0
-            print('else... ' + str(i) + ', ' + str(current_streak) + ', ' + str(longest_streak))
+        
+        # to ID the ending index where the best streak occurred. 
+        if best_streak == current_streak: 
+            best_streak_loc = i
 
-        if longest_streak == current_streak:
-            longest_streak_loc = i
-
-    print('longest streak of {}, ends at row index: {}'.format(longest_streak, longest_streak_loc)) 
-
+    print('longest streak of {}, ends at row index: {}'.format(best_streak, best_streak_loc))
+    
+    return best_streak
 
 
 
 # Testing #####################################################################
-#          0 1 2 3 4 5 6 7 8 9
-flips = [0,1,0,1,1,1,0,1,0,0] # length 10
-
-#            0 1 2 3 4 5 6 7 8 9 1 1 2 3 4 5 6 7 8 9
-flips20 = [0,1,0,1,1,1,0,1,0,0,1,1,1,1,0,0,0,1,1,1] # length 20
-
 import numpy as np
 np.random.seed(123)
 flips1000 = [np.random.randint(0,2) for n in range(1000)]
 
-
-print(longest_streak(flips, 1))
-print(longest_streak(flips20, 1))
 print(longest_streak(flips1000, 1))
 
+# inspect underlying data to validate results
 for i, val in enumerate(flips1000):
-    if i < 500:
+    if i < 450:
         print(i, val, sep = ': ')
-
-    
