fef2e48ea60ddfc3b3df36d6cf61c4e34d97af53
BrandonHoeft <brandonmhoeft@gmail.com>
2018-06-18 20:26:14 -0500

    in which our hero revises the function to instead  check whether the value at i in the list is equal  to a specified parameter value. Able to complete  the function and do some initial testing on lists  of size 10, 20, 1000 of 1's and 0's. Current script  includes test code and logging outputs from func.  Will delete these in later commit.

--- longest_streak.py
+++ longest_streak.py
@@ -1,20 +1,46 @@
+def longest_streak(var, value):
 
-actions = [0,1,0,1,1,1,0,1,0,0]
+    current_streak = 0
+    longest_streak = 0
+    longest_streak_loc = 0
 
-def longest_streak(var):
+    for i in range(len(var)):
+        if var[i] == value:
+            current_streak += 1
+            longest_streak = max(longest_streak, current_streak)
+            print('if... ' + str(i) + ', ' + str(current_streak) + ', ' + str(longest_streak))
 
-    streak = 1
-    best_streak = 1
-    best_streak_i = 0
-
-    for i in range(len(var)-1):
-        print(str(i) + ' iteration') # testing/scaffolding
-        if var[i] == var[i + 1] and streak >= best_streak:
-            streak += 1
-            best_streak = streak
-            best_streak_i = i
         else:
-            streak = 1 # reset streak
-        print(str(streak))
+            longest_streak = max(longest_streak, current_streak)
+            current_streak = 0
+            print('else... ' + str(i) + ', ' + str(current_streak) + ', ' + str(longest_streak))
 
-    print('longest streak of {}, starts at index: {}'.format(best_streak, best_streak_i)) # testing/scaffolding
+        if longest_streak == current_streak:
+            longest_streak_loc = i
+
+    print('longest streak of {}, ends at row index: {}'.format(longest_streak, longest_streak_loc)) 
+
+
+
+
+# Testing #####################################################################
+#          0 1 2 3 4 5 6 7 8 9
+flips = [0,1,0,1,1,1,0,1,0,0] # length 10
+
+#            0 1 2 3 4 5 6 7 8 9 1 1 2 3 4 5 6 7 8 9
+flips20 = [0,1,0,1,1,1,0,1,0,0,1,1,1,1,0,0,0,1,1,1] # length 20
+
+import numpy as np
+np.random.seed(123)
+flips1000 = [np.random.randint(0,2) for n in range(1000)]
+
+
+print(longest_streak(flips, 1))
+print(longest_streak(flips20, 1))
+print(longest_streak(flips1000, 1))
+
+for i, val in enumerate(flips1000):
+    if i < 500:
+        print(i, val, sep = ': ')
+
+    
